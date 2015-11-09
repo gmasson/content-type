9fdaa70e090ad4ef723822eb2083f6a13b8db6fd
Gabriel Masson <gmasson@users.noreply.github.com>
2015-11-09 19:18:16 -0200

    Create img.php

---
+++ img.php
@@ -0,0 +1,14 @@
+<?php
+  # JPEG(jpg):
+  header('Content-Type: image/jpeg');
+  /*
+  # PNG:
+  header('Content-Type: image/png');
+  
+  # BMP:
+  header('Content-Type: image/bmp');
+  */
+  
+  # Path Image
+  echo '1.jpg';
+?>
